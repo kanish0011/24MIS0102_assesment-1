92d9456ea21f1fa58ae8235716ac86ce1a5f2f12
Kanish0011 <k93730057@gmail.com>
2026-07-18 23:39:21 +0530

    Added login feature module

--- appointment.py
+++ appointment.py
@@ -1,2 +1,5 @@
 print("Hospital Appointment Booking System")
-print("Patient Registration Module Loaded")#added line git 
+print("Patient Registration Module Loaded")#added line 
+#Added logical functionality 
+def login():
+    print("user login system")
